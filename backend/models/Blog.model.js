f3d98d879c0d7afec3bef38ad338679c9759b0a5
Sumit Beniwal <beniwalsumit1412@gmail.com>
2022-08-11 22:35:26 +0530

    updating blog app

--- backend/models/Blog.model.js
+++ backend/models/Blog.model.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const BlogSchema = mongoose.Schema({
     authorId: { type: String, required: true },
     title: { type: String, required: true },
-    subTitle: { type: String, required: true },
-    body: { type: String, required: true },
+    headings: { type: [String], required: true },
+    body: { type: [String], required: true },
     date: { type: String, required: true },
-    tags: { type: Array, required: true },
+    tags: { type: String, required: true },
 })
 
 const BlogModel = mongoose.model("blog", BlogSchema);
