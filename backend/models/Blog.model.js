d5a6bfec04ecac568cc343b9c5f0641fef621141
Sumit Beniwal <beniwalsumit1412@gmail.com>
2022-08-11 23:49:18 +0530

    added demo blogs in db

--- backend/models/Blog.model.js
+++ backend/models/Blog.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const BlogSchema = mongoose.Schema({
     authorId: { type: String, required: true },
     title: { type: String, required: true },
+    img: {type: String},
     body: { type: [String], required: true },
     date: { type: String, required: true },
     tags: { type: String, required: true },
