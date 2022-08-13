3af6c644181ee4a05369796c42fdaa54c0be9bcb
Sumit Beniwal <beniwalsumit1412@gmail.com>
2022-08-13 12:12:10 +0530

    complete the carousel

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -40,32 +40,63 @@ const Data = [
 ];
 
 function Carousels() {
-  
   return (
-    <Carousel variant="dark" fade>
+    <Carousel variant="dark" fade interval={2000000}>
       <Carousel.Item>
-        <div>
-        <img
-          className="d-block"
-          src="https://assets.website-files.com/601b0eabbce5fc78dc318621/601e98759239ef2558d88222_Main%20Post%20Image%208.png"
-          alt="First slide"
-        />
-          <h2 style={{float:"right"}}>Some Heading</h2>
+        <div className={styles.parent}>
+          <img
+            className="d-block"
+            src="https://assets.website-files.com/601b0eabbce5fc78dc318621/601e98759239ef2558d88222_Main%20Post%20Image%208.png"
+            alt="First slide"
+          />
+          <div className={styles.child}>
+            <h3>
+              <div className={styles.line}></div>Travel
+            </h3>
+            <h2>
+              How to find a place to work remotely from as a digital nomad
+            </h2>
+            <p>
+              October 22, 2020 <strong>•</strong> 7 min read
+            </p>
+          </div>
         </div>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-50 h-100"
-          src="https://assets.website-files.com/601b0eabbce5fc78dc318621/60302d12c924de6cd28f2b41_Main%20Post%20Image%2011.png"
-          alt="Second slide"
-        />
+        <div className={styles.parent}>
+          <img
+            className="d-block"
+            src="https://assets.website-files.com/601b0eabbce5fc78dc318621/60302d12c924de6cd28f2b41_Main%20Post%20Image%2011.png"
+            alt="First slide"
+          />
+          <div className={styles.child}>
+            <h3>
+              <div className={styles.line}></div>Tech
+            </h3>
+            <h2>“What it is” and “What it does” thinking.</h2>
+            <p>
+              July 16, 2022 <strong>•</strong> 4 min read
+            </p>
+          </div>
+        </div>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-50 h-100"
-          src="https://assets.website-files.com/601b0eabbce5fc78dc318621/6030302271aa6a1af8d25e53_Main-Post-Image-12.jpg"
-          alt="Third slide"
-        />
+        <div className={styles.parent}>
+          <img
+            className="d-block"
+            src="https://assets.website-files.com/601b0eabbce5fc78dc318621/6030302271aa6a1af8d25e53_Main-Post-Image-12.jpg"
+            alt="First slide"
+          />
+          <div className={styles.child}>
+            <h3>
+              <div className={styles.line}></div>Travel
+            </h3>
+            <h2>What They Don’t Say About Ontario, Canada.</h2>
+            <p>
+              August 6, 2022 <strong>•</strong> 8 min read
+            </p>
+          </div>
+        </div>
       </Carousel.Item>
     </Carousel>
   );
