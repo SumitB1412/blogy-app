01b76c19306eaf6d8c23718bf1fdb2846593e8a4
Sumit Beniwal <beniwalsumit1412@gmail.com>
2022-08-14 17:20:22 +0530

    update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "../styles/navbar.module.css";
-import { RiAccountCircleLine } from 'react-icons/ri';
-import { GrAdd } from 'react-icons/gr';
-import { BsSearch } from 'react-icons/bs';
+import { RiAccountCircleLine } from "react-icons/ri";
+import { GrAdd } from "react-icons/gr";
+// import { BsSearch } from "react-icons/bs";
 
 export const Navbar = () => {
   return (
@@ -20,9 +20,17 @@ export const Navbar = () => {
         <p>Food</p>
       </div>
       <div>
-        <RiAccountCircleLine fontSize="24px" style={{marginRight:"10%"}} />
-        <GrAdd fontSize="22px" style={{marginRight:"5%"}} />
-        <BsSearch fontSize="18px" style={{marginRight:"5%"}}/>
+        <RiAccountCircleLine
+          fontSize="22px"
+          style={{ marginRight: "5%" }}
+          cursor="pointer"
+        />
+        <GrAdd fontSize="22px" style={{ marginRight: "5%" }} cursor="pointer" />
+        {/* <BsSearch
+          fontSize="18px"
+          style={{ marginRight: "5%" }}
+          cursor="pointer"
+        /> */}
       </div>
     </div>
   );
