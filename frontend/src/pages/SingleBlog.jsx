1012f16e80a66a12162ce49aee15b4acda915578
SumitB1412 <beniwalsumit1412@gmail.com>
2022-09-09 01:05:28 +0530

    update coment box

--- frontend/src/pages/SingleBlog.jsx
+++ frontend/src/pages/SingleBlog.jsx
@@ -3,16 +3,17 @@ import styles from "../styles/singleblog.module.css";
 import { BiTimeFive } from "react-icons/bi";
 import { useSelector } from "react-redux";
 import { RiAccountCircleLine } from "react-icons/ri";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
-import Form from "react-bootstrap/Form";
+// import FloatingLabel from "react-bootstrap/FloatingLabel";
+// import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export const SingleBlog = () => {
-  const [comment, setComment] = useState("");
+  // const [comment, setComment] = useState("");
+  const [flag, setFlag] = useState(false);
   const { singleBlog } = useSelector((store) => store.blogs);
   const postComment = () => {
-    console.log(comment);
-  }
+    // console.log(comment);
+  };
   return (
     <>
       <div className={styles.thanos}>
@@ -37,28 +38,27 @@ export const SingleBlog = () => {
       </div>
       <div className={styles.commentBox}>
         <div>
+          <h2>Comments</h2>
+        </div>
+        <div>
           <div>
             <RiAccountCircleLine
               fontSize="30px"
-              style={{ marginRight: "20px", marginLeft: "10px" }}
+              style={{ marginRight: "20px" }}
             />
           </div>
-          <div>
-            <FloatingLabel controlId="floatingTextarea2" label="Message">
-              <Form.Control
-                as="textarea"
-                placeholder="Leave a comment here"
-                style={{ height: "100px" }}
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-              />
-            </FloatingLabel>
+          <div className={styles.textAreaDiv}>
+            <textarea
+              type="text"
+              placeholder="Write a comment..."
+              onClick={() => setFlag(true)}
+            />
           </div>
         </div>
-        <div className={styles.buttonBox}>
-          <Button variant="outline-danger">Cancel</Button>
+        {/* <div className={styles.buttonBox}>
+          <Button variant="outline-danger" onClick={()=>setFlag(false)}>Cancel</Button>
           <Button variant="outline-primary" onClick={postComment}>Post Comment</Button>
-        </div>
+        </div> */}
       </div>
     </>
   );
